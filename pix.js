8f74faff414ce20757a9b26aa4aa15e50ad4fb7a
Francisco-AMenezes <franciscoalvesmenezes90@gmail.com>
2024-07-22 14:13:19 -0300

    Correção do valor de todos os parâmetros.

--- pix.js
+++ pix.js
@@ -1,11 +1,11 @@
 import { Pix } from "faz-um-pix";
 import clipboard from "clipboardy";
 
-const key = chave;
-const destination = destino;
-const city = cidade;
-const event = evento;
-const value = valor;
+const key = process.argv[2]
+const destination = process.argv[3]
+const city = process.argv[4]
+const event = process.argv[5]
+const value = process.argv[6]
 
 const code = await Pix(key, destination, city, value, event, false);
 
